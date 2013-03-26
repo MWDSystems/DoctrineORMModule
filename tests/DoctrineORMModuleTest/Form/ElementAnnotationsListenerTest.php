7df8ad06c79011c50d9c923aa3bfa9624e03b0fd
Kyle Spraggs <theman@spiffyjr.me>
2013-03-26 11:03:17 -0500

    Added ToMany and ToOne support to the ElementAnnotationsListener.

--- tests/DoctrineORMModuleTest/Form/ElementAnnotationsListenerTest.php
+++ tests/DoctrineORMModuleTest/Form/ElementAnnotationsListenerTest.php
@@ -7,22 +7,23 @@ use Doctrine\ORM\Mapping\Column;
 use DoctrineORMModule\Form\Annotation\ElementAnnotationsListener;
 use PHPUnit_Framework_TestCase;
 use Zend\EventManager\Event;
+use DoctrineORMModuleTest\Framework\TestCase;
 
 /**
  * Description of ElementAnnotationsListenerTest
  *
  * @author otterdijk
  */
-class ElementAnnotationsListenerTest extends PHPUnit_Framework_TestCase
+class ElementAnnotationsListenerTest extends TestCase
 {
     /**
      * @dataProvider eventProvider
      */
     public function testHandleAnnotationType($type, $expectedType)
     {
-        $listener                 = new ElementAnnotationsListener();
-        $event                    = new Event();
-        $checkboxAnnotation       = new Column();
+        $listener = new ElementAnnotationsListener($this->getEntityManager());
+        $event = new Zend\EventManager\Event();
+        $checkboxAnnotation = new Doctrine\ORM\Mapping\Column();
         $checkboxAnnotation->type = $type;
 
         $event->setParam('annotation', $checkboxAnnotation);
@@ -33,12 +34,53 @@ class ElementAnnotationsListenerTest extends PHPUnit_Framework_TestCase
 
         $this->assertEquals($expectedType, $spec['spec']['type']);
     }
+    
+    public function testToOneReturnsEntitySelect()
+    {
+        $listener   = new ElementAnnotationsListener($this->getEntityManager());
+        $event      = new Zend\EventManager\Event();
+        $annotation = new Doctrine\ORM\Mapping\ManyToOne();
+        $annotation->targetEntity = 'DoctrineORMModuleTest\Assets\Entity\Category';
+
+        $event->setParam('annotation', $annotation);
+        $event->setParam('elementSpec', new ArrayObject(array(
+            'spec' => array(
+                'attributes' => array(
+                    'class' => 'foo'
+                )
+            )
+        )));
+
+        $listener->handleToOneAnnotation($event);
+        $spec = $event->getParam('elementSpec');
+        $this->assertArrayNotHasKey('multiple', $spec['spec']['attributes']);
+        $this->assertEquals('DoctrineORMModule\Form\Element\EntitySelect', $spec['spec']['type']);
+        $this->assertEquals('DoctrineORMModuleTest\Assets\Entity\Category', $spec['spec']['options']['target_class']);
+    }
+    
+    public function testToManyReturnsEntitySelect()
+    {
+        $listener   = new ElementAnnotationsListener($this->getEntityManager());
+        $event      = new Zend\EventManager\Event();
+        $annotation = new Doctrine\ORM\Mapping\ManyToMany();
+        $annotation->targetEntity = 'DoctrineORMModuleTest\Assets\Entity\Category';
+
+        $event->setParam('annotation', $annotation);
+        $event->setParam('elementSpec', new ArrayObject());
+
+        $listener->handleToManyAnnotation($event);
+        $spec = $event->getParam('elementSpec');
+        $this->assertArrayHasKey('multiple', $spec['spec']['attributes']);
+        $this->assertEquals('DoctrineORMModule\Form\Element\EntitySelect', $spec['spec']['type']);
+        $this->assertEquals('DoctrineORMModuleTest\Assets\Entity\Category', $spec['spec']['options']['target_class']);
+    }
 
     public function testHandleAnnotationAttributesShallAppend()
     {
-        $listener         = new ElementAnnotationsListener();
-        $event            = new Event();
-        $annotation       = new Column();
+        $listener = new ElementAnnotationsListener($this->getEntityManager());
+        $event = new Zend\EventManager\Event();
+        $annotation = new Doctrine\ORM\Mapping\Column();
+
         $annotation->type = 'text';
 
         $event->setParam('annotation', $annotation);
